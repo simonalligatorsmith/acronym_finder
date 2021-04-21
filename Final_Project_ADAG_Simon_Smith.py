eb4b49a6f819382c778d75ba21141d0b069c0534
simonalligatorsmith <56604094+simonalligatorsmith@users.noreply.github.com>
2021-04-21 17:39:52 -0600

    Prettified for submission
    Deleted extraneous comments and fluff.

--- Final_Project_ADAG_Simon_Smith.py
+++ Final_Project_ADAG_Simon_Smith.py
@@ -1,14 +1,12 @@
 # Author: Simon Smith
+"""
+Input: path to xpdf reader, path to folder with .txt or .pdf files
+Process: discover acronyms (parenthetically enclosed) and the phrase they represent
+Output: print to screen the matches of acronym and phrase
 
-"""
-(for each text)
-0. read text from PDF into text file
-1. identify acronyms, gather surrounding text
-2. guess acronym meanings
-3. print pretty list of acronyms
-"""
-"""
+Note:
 Requires Xpdf reader and path to its executable
+Also, the program can be a little slow with longer files - just give it a minute to think!
 """
 
 import os
@@ -18,7 +16,7 @@ import re
 from nltk.corpus import stopwords as stopword_file
 
 # set to true for debug info
-Verbose = True
+Verbose = False
 
 # initialize stopwords
 stopwords = set(stopword_file.words('english'))
@@ -66,13 +64,13 @@ def find_acronyms(text_path):
         file_text = infile.read()
         acronym_and_phrase = []
 
-        # add sequences of a string of 2 or more capital letters
-        #capital_sequences = re.findall(r'[A-Z]{2,}',file_text)
 
         # add parenthetical acronyms (capitals, whtspace in parentheses with optional -s), stripped of parentheses
         # ex, {'LDV', 'ODEs', 'PAM', 'ICTV', 'DTP', 'SHFV', 'PRRSV', 'NTRs', 'EAV'}
         parenthetical_sequences = set([phrase.strip('()') for phrase in re.findall(r'\([-A-Zs\s]*\)',file_text)])
         parenthetical_sequences = [item for item in parenthetical_sequences if len(item) > 1]
+        if Verbose:
+            print("Parenthetical sequences: ",parenthetical_sequences,'\n')
 
         # for an acronym of N letters, find up to N+N-1 words before and after the acronym in the text
         for acronym in parenthetical_sequences:
@@ -84,8 +82,7 @@ def find_acronyms(text_path):
             search_radius = 2*len(acronym)-1
             # below term is any word or words up to the search radius plus the acronym
             regex_search_term = search_radius * (r'(\b\w*\b[-\s])?') + '\(' + acronym
-            # the result of the regex is a list, all toLower
-            #print("Regex results: ",[item for item in re.findall( regex_search_term, file_text )])
+            # the result of the regex is a list, all made lowercase
 
             # regex results based on regex_search_term. May return more than one term: [(first)(second)]
             for phrase in re.findall( regex_search_term, file_text ):
@@ -96,15 +93,23 @@ def find_acronyms(text_path):
                 # if the capture cell isn't blank, grab the initial (made lowercase) of each word, join as a string
                 initials = ''.join([initial[0].upper() for initial in list(phrase) if initial])
 
-                print("Phrase: ",phrase," Initials: ",initials," Acronym:", acronym)
-                print(' '.join(phrase))
+                if Verbose:
+                    print("Phrase: ",phrase," Initials: ",initials," Acronym:", acronym)
+                    print(' '.join(phrase))
 
+                # find appropriate phrase that acronym represents and store it to return later #
 
-                # find appropriate phrase that acronym represents and store it to return later#
+                # if no letters from the acronym are in the initials for the phrase, move on to the next word
+                if not [letter for letter in initials if letter in acronym]:
+                    if Verbose:
+                        print("Skipped acronym ",acronym," for phrase ",phrase)
+                    continue
 
-                # if the initials are fewer than the acronym, no dice.
+                # if the initials of the phrase are fewer than the acronym, then move on; it's not a match
                 #   Ex, Initials:  EV  Acronym: LDV
-                if len(initials) < len(acronym):
+                elif len(initials) < len(acronym):
+                    if Verbose:
+                        print("Skipped acronym ",acronym," for phrase ",phrase)
                     continue
 
                 # if the initials equal the acronym, or the acronym with a lowercase s, return the phrase buffer
@@ -118,29 +123,13 @@ def find_acronyms(text_path):
                 elif initials.endswith(acronym):
                     # append to the acronym_and_phrase bank the acronym, and the last (length of acronym) words in phrase
                     acronym_and_phrase.append([acronym, phrase[-len(acronym):]])
-                    #print([acronym, phrase[-len(acronym):]])
 
-                # if the phrase has stopwords, try matching the acronym to the stopword-less phrase
-                #   Ex, Initials:  ICOTTOV  Acronym: ICTV   Phrase:  ['International ', 'Committee ', 'on ', 'the', 'Taxonomy ', 'of ', 'Viruses ']
                 else:
-                    # try matching initials in order, with whatever crap comes between them
-                    # go through the letters of the INITIALS one by one, asking if they're the next letter of ACRONYM
 
-                    # for iterating through letters of the acronym
-                    acronym_index = 0
+                    continue
 
-                    # for iterating through initial letters of the phrase, to compare to acronym letters
-                    for index in range(len(initials)):
-                        if initials[index] == acronym[acronym_index]:
-                            # see if the next acronym letter is in the target phrase
-                            acronym_index += 1
-
-                        # if the length of the index is the size of its length, it's all been found! Report it!
-                        if acronym_index == len(acronym):
-                            print("Final product for final ELSE test: ",[acronym, phrase[:index]])
-
-
-            print() #formatting
+            if Verbose:
+                print() #formatting
 
     return acronym_and_phrase
 
@@ -149,39 +138,8 @@ for file in os.listdir(source_path):
     # only look at the generated (or original) .txt files, not the pdfs!
     if file.endswith('.txt'):
         txt_abs_path = source_path + '\\' + file
-        if Verbose:
-            print("\nFinding acronyms for ",txt_abs_path)
-        print(find_acronyms(txt_abs_path))
-
-
-
-"""
-Notes on defining acronomy:
-    The most common way to do acronyms is like so
-    where '1' is what the acronym stands for, and
-    '2' is the acronym itself
-    
-        1 (2) 
-        
-        1 can be...
-        separated by hyphens:
-            "definition-theorem-proof (DTP)"
-        capitalized:
-            "International Committee on the Taxonomy of Viruses (ICTV)"
-        or space-separated:
-            "equine arteritis virus (EAV)" 
-        of mixed space and hyphen separated:
-            "non-translated regions (NTRs)"
-        or more words than the acronym, especially stopwords:
-            "porcine reproductive and respiratory syndrome virus (PRRSV)"
-            ('and' disappears)
-            
-        2 can have...
-        a lowercase 's' on the end for plurality
-            "non-translated regions (NTRs)"
-        fewer letters than there are in the represented phrase, especially stopwords
-            "porcine reproductive and respiratory syndrome virus (PRRSV)"
-            ('and' disappears)
-            
-        
-"""
+        print("\n**** Finding acronyms for ",txt_abs_path,' ***')
+        print("Matches: ")
+        for ac in find_acronyms(txt_abs_path):
+            print(ac)
+print("Finished!")
